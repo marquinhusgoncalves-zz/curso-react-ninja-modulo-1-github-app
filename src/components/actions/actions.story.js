e6c498d882c013264fb399a5491e6c65f8ed866f
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-03-19 20:00:18 -0300

    M2#A43

---
+++ src/components/actions/actions.story.js
@@ -0,0 +1,8 @@
+import React from 'react'
+// import {storiesOf} from '@kadira/storybook'
+import { storiesOf } from '@storybook/react'
+
+storiesOf('Div', module)
+  .add('first story', () => (
+    <div>Hello</div>
+  ))
--- src/components/title/index.js
+++ src/components/title/index.js
@@ -3,7 +3,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
-import style from './title.css'
+// import style from './title.css'
 
 const Title = ({ children }) => (
   <h1>{children}</h1>
