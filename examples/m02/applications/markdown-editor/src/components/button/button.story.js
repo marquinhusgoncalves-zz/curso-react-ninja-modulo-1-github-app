aaf852fabfefbf0a8bd1afd95e1be98b06003cae
Marquinhus Gonçalves <marcus.goncalves@loggi.com>
2019-04-21 20:17:44 -0300

    M2#A115 - Aplicação: editor de Markdown - storybook do botão

---
+++ examples/m02/applications/markdown-editor/src/components/button/button.story.js
@@ -0,0 +1,25 @@
+'use strict'
+
+import React from 'react'
+import { storiesOf, action } from '@storybook/react'
+import Button from './index'
+
+const stories = storiesOf('Button', module)
+
+stories.addDecorator((story) => (
+  <div style={{display: 'flex', height: 40}}>
+    {story()}
+  </div>
+))
+
+stories.add('Button success', () => (
+  <Button onClick={action('sucess')} kind='success'>
+    Success
+  </Button>
+))
+
+stories.add('Button danger', () => (
+  <Button onClick={action('danger')} kind='danger'>
+    Danger
+  </Button>
+))
